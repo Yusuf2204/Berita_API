b70ef5088902acb3bcf94b59dd82d4f300eacbbf
Yusuf2204 <muhammadyusuf667788@gmail.com>
2024-05-16 15:58:58 +0700

    Add endpoint creat post

--- app/Http/Controllers/Posts/PostsController.php
+++ app/Http/Controllers/Posts/PostsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostsResource;
 use App\Http\Resources\PostsDetailResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Posts\Posts;
 
 class PostsController extends Controller
@@ -16,13 +17,13 @@ class PostsController extends Controller
 
     public function index()
     {
-        $posts = Posts::all();
+        $posts = $this->model::with('writer:userId,userName')->get();
 
         $data = array(
             'data' => $posts
         );
 
-        return PostsResource::collection($posts);
+        return PostsDetailResource::collection($posts);
     }
 
     public function show($id)
@@ -31,5 +32,19 @@ class PostsController extends Controller
 
         $data_id = new PostsDetailResource($post);
         return $data_id;
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'postTitle' => 'required|max:255',
+            'postNews' => 'required',
+        ]);
+
+        $request['postUserId'] = Auth::user()->userId;
+
+        $post = $this->model::create($request->all());
+        $data_post = new PostsDetailResource($post->loadMissing('writer:userId,userName'));
+        return $data_post;
     }
 }
